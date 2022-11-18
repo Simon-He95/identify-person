10efc028685e5577ce90fa42e8ee6fe831fe85cf
Simon He <13917107469@163.com>
2022-11-18 15:55:39 +0800

    chore: add download

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import { download } from 'simon-js-tool'
 import identifyPerson from '../../src'
 const color = ref('#0066cc')
 const canvasBg = ref()
 const cvs = ref()
 const video = ref()
 const image = ref()
+const disabled = ref(true)
 const isLoading = ref(true)
 let changeBgColor: (color: string) => void
 onMounted(async () => {
@@ -20,7 +22,9 @@ function change() {
 }
 function takePhoto() {
   image.value.src = cvs.value.toDataURL('image/png')
+  disabled.value = false
 }
+const clickHandler = () => download(image.value.src)
 </script>
 
 <template>
@@ -34,10 +38,13 @@ function takePhoto() {
     <img ref="image" src="">
   </div>
   <div flex="~ gap3" justify-center mt4>
-    <button @click="takePhoto">
+    <button btn @click="takePhoto">
       拍照
     </button>
     <input v-model="color" type="color" @input="change">
+    <button :disabled="disabled" btn @click="clickHandler">
+      下载
+    </button>
   </div>
 </template>
 
